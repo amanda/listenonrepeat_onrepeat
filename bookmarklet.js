fa1bd3751979242b5c16e9ebfd433a8844f25549
amandapickering <pickering.amanda@gmail.com>
2015-03-03 22:01:59 -0500

    ok really this time

--- bookmarklet.js
+++ bookmarklet.js
@@ -7,4 +7,4 @@
 })();
 
 // bookmarklet
-javascript:(function()%7B(function%20()%20%7Bif%20(location.search%20!%3D%3D%20%22%22)%20%7Bvar%20localURL%20%3D%20%22http%3A%2F%2Flistenonrepeatonrepeat.herokuapp.com%2F%22%20%2B%20location.search%3Blocation%20%3D%20localURL%3B%7D%7D)()%7D)()
+javascript:(function()%7B(function%20()%20%7Bif%20(location.search%20!%3D%3D%20%22%22)%20%7Bvar%20localURL%20%3D%20%22http%3A%2F%2Flistenonrepeatonrepeat.herokuapp.com%2Fwatch%22%20%2B%20location.search%3Blocation%20%3D%20localURL%3B%7D%7D)()%7D)()
